ab0c54b69b9aef7c26b97e895c114521440db6f1
Lou <luana.martelli@heig-vd.ch>
2017-10-03 12:40:20 +0200

    Added some modificiation

--- test/github-api.js
+++ test/github-api.js
@@ -1,22 +1,20 @@
 const chai = require('chai');
+const dirty = require('dirty-chai');
 const request = require('superagent');
 const { username, token } = require('../github-credentials.json');
 
 const should = chai.should();
+chai.use(dirty);
 
 describe('Github API', () => {
-    it('gets me the avatar of a user', (done) =>  {
-        const owner = 'LuanaMartelli';
-        const url = `https://api.github.com/users/${owner}`
-        request
-        .get(url)
-        .auth(username, token)
-        .set("Accept", "application/vnd.github.v3+json")
-        .end((err, res) => {
-            should.not.exist(err);
-            should.exist(res);
-            done();
-        })
+  it('gets me the avatar of a user', (done) => {
+    const owner = 'LuanaMartelli';
+    const url = `https://api.github.com/users/${owner}`;
+    request.get(url).auth(username, token).set('Accept', 'application/vnd.github.v3+json').end((err, res) => {
+      should.not.exist(err);
+      should.exist(res);
+      done();
     });
+  });
 });
 
