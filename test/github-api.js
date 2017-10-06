bf2be7f1cd410edaaeb30cc41f307d22e952c64f
Mikijonie <mika.pagani@heig-vd.ch>
2017-10-06 11:12:47 +0200

    Get avatar works (but doesn't save)

--- test/github-api.js
+++ test/github-api.js
@@ -7,13 +7,18 @@ const should = chai.should();
 chai.use(dirty);
 
 describe('Github API', () => {
-  it('gets me the avatar of a user', (done) => {
+  it('Gets me the avatar (image data) of a given user', (done) => {
     const owner = 'LuanaMartelli';
     const url = `https://api.github.com/users/${owner}`;
     request.get(url).auth(username, token).set('Accept', 'application/vnd.github.v3+json').end((err, res) => {
       should.not.exist(err);
       should.exist(res);
-      done();
+      const avatar = res.body.avatar_url;
+      request.get(avatar).auth(username, token).set('Accept', 'image/*').end((err2, res2) => {
+        should.not.exist(err2);
+        should.exist(res2);
+        done();
+      });
     });
   });
 });
